9aeda42374dbd0458e1f24711ed7fb6eb826f9a3
Luke <dev@crimsonxv.pro>
2018-03-02 17:50:59 +0000

    Only launch next instance when previous ready
    This should yield faster logins as the instances won't be fighting to login as only one will be logging in at a time

--- aio_launcher.py
+++ aio_launcher.py
@@ -1,50 +1,78 @@
-import math
-import sys
 import asyncio
-from multiprocessing import Process
+import math
+import os
+import shutil
+import subprocess
+import sys
+from multiprocessing import Process, Pipe
 
 import instance
 
-if __name__ == "__main__":
-    shards_per_instance = 2
-    total_shards = 2
 
-    shards = []
+loop = asyncio.get_event_loop()
+executable = str(shutil.which('python3.6') or shutil.which('py')).split('/')[-1]
+instance_queue = []
+instances = []
 
-    if "--sharded" in sys.argv:
-        print("Sharding enabled. Validating shard count...")
-        if total_shards >= 40 and total_shards % 16 != 0:  # 40 * 2,500 = 100,000 (see: https://github.com/discordapp/discord-api-docs/issues/387)
-            print("Bad shard count: total_shards must be a multiple of 16")
-            sys.exit(0)
 
-        instances = math.ceil(total_shards / shards_per_instance)
-        print(f'Using {instances} instances')
+class PendingInstance:
+    def __init__(self, i_id, total_shards, shard_ids):
+        self.id = i_id
+        self.total_shards = total_shards
+        self.shard_ids = shard_ids
 
-        for i in range(0, instances):
-            start = i * shards_per_instance
-            last = start + shards_per_instance
 
-            if last > total_shards:
-                last = total_shards
-
-            print(f"[{i}] Creating bot instance (Shards {start}-{last})")
-            ids = list(range(start, last))
-
-            p = Process(target=instance.Instance, args=(i, total_shards, ids,))
-            shards.append(p)
-            p.start()
-
-    else:
-        p = Process(target=instance.Instance, args=(0, 1, [0],))
-        shards.append(p)
+def launch_next_shard():
+    if instance_queue:
+        i = instance_queue.pop(0)
+        print('Launching instance {}'.format(i.id))
+        listen, send = Pipe()
+        p = Process(target=instance.Instance, args=(i.id, i.total_shards, i.shard_ids, send,))
+        instances.append(p)
         p.start()
 
-    loop = asyncio.get_event_loop()
+        if listen.recv() == 1:
+            launch_next_shard()
+        listen.close()
+    else:
+        print('All instances launched!')
 
-    print('All instances launched.')
 
-    async def waiter():
-        await asyncio.sleep(5)
+def wait(delay: int):
+    loop.run_until_complete(asyncio.sleep(delay))
 
-    while True:  # Keep the main process up
-        loop.run_until_complete(waiter())
+
+if __name__ == '__main__':
+    shards_per_instance = 1
+    total_shards = 2
+
+    sharded = '--sharded' in sys.argv
+
+    if sharded:
+        print('Sharding enabled. Validating shard count...')
+        if total_shards >= 40 and total_shards % 16 != 0:  # 40 * 2,500 = 100,000 (see: https://github.com/discordapp/discord-api-docs/issues/387)
+            print('Bad shard count: total_shards must be a multiple of 16')
+            sys.exit(0)
+
+        total_instances = math.ceil(total_shards / shards_per_instance)
+        print('Using {} instances'.format(total_instances))
+
+        wait(5)
+
+        for i in range(0, total_instances):
+            start = i * shards_per_instance
+            last = min(start + shards_per_instance, total_shards)
+            ids = list(range(start, last))
+
+            print('Appending instance {} to launch queue...'.format(i))
+            instance_queue.append(PendingInstance(i, total_shards, ids))
+    else:
+        instance_queue.append(PendingInstance(0, 1, [0], launch_next_shard))
+
+    launch_next_shard()
+
+    try:
+        while True:  # Keep the main process up
+            wait(5)
+    except KeyboardInterrupt:
+        pass
--- instance.py
+++ instance.py
@@ -1,32 +1,43 @@
 import json
 import os
+from datetime import datetime
 
-from discord.ext.commands import AutoShardedBot, when_mentioned_or as wmo
-from utils import permissions
+import discord
+from discord.ext.commands import when_mentioned_or as wmo
+from discord.ext.commands import AutoShardedBot
+from IPC import interface
 
 
 class Instance:
-    def __init__(self, instance, shard_count, ids):
+    def __init__(self, instance, shard_count, ids, pipe):
+        self.pipe = pipe
+
         with open('config.json') as r:
             config = json.load(r)
 
-        self.bot = AutoShardedBot(command_prefix=wmo(config.get('prefix')), shard_count=shard_count, shard_ids=ids, help_attrs=dict(Hidden=True), pm_help=True, fetch_offline_members=False)
+        self.bot = AutoShardedBot(command_prefix=wmo(config.get('prefix')), shard_count=shard_count, shard_ids=ids,
+                                  help_attrs=dict(Hidden=True), pm_help=True, fetch_offline_members=False)
 
         self.bot.prefix = config.get('prefix')
         self.bot.instance = instance
+        self.bot.startup = datetime.now()
 
         self.bot.add_listener(self.on_ready)
         self.bot.run(config.get('token'))
 
     async def on_ready(self):
-        print(f'[Instance-{self.bot.instance}] Ready!')
         for file in os.listdir("cogs"):
             if file.endswith(".py"):
                 name = file[:-3]
                 self.bot.load_extension(f"cogs.{name}")
 
+        print(f'[Instance-{self.bot.instance}] Ready!')
+        self.pipe.send(1)
+        self.pipe.close()        
+
     async def on_message(self, msg):
-        if not self.bot.is_ready() or msg.author.bot or not permissions.can_send(msg):
-            return
+        if not self.bot.is_ready() or msg.author.bot or \
+                not (isinstance(msg.channel, discord.DMChannel) or msg.channel.permissions_for(msg.guild.me).send_messages):
+                return
 
         await self.bot.process_commands(msg)
