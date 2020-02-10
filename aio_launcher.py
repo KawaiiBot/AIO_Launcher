ea2e2075a809ddea80b5e1d3b9be7ed83c57e785
Devoxin <lukel9914@gmail.com>
2020-02-10 15:08:11 +0000

    stuff n thangs

--- aio_launcher.py
+++ aio_launcher.py
@@ -1,78 +1,124 @@
 import asyncio
 import math
-import os
 import shutil
-import subprocess
-import sys
-from multiprocessing import Process, Pipe
+from datetime import datetime
+from multiprocessing import Pipe, Process, cpu_count
+from multiprocessing.connection import Connection
+from threading import Thread
+from time import sleep
 
-import instance
-
+from instance import Instance
 
 loop = asyncio.get_event_loop()
 executable = str(shutil.which('python3.6') or shutil.which('py')).split('/')[-1]
-instance_queue = []
-instances = []
+
+###################################################
+#                   INFORMATION                   #
+###################################################
+# :: op codes                                     #
+#  status: A status update from the instance.     #
+#          CONNECTED - All shards are connected.  #
+#                                                 #
+###################################################
 
 
-class PendingInstance:
-    def __init__(self, i_id, total_shards, shard_ids):
-        self.id = i_id
-        self.total_shards = total_shards
-        self.shard_ids = shard_ids
+def log(name, severity, message, *args):
+    dt_format = '%H:%M:%S'
+    current_time = datetime.now().strftime(dt_format)
+    formatted = message.format(*args)
+    full_message = f'[{current_time}] [{severity.upper()}] {name.title()}: {formatted}'
+    print(full_message)
 
 
-def launch_next_shard():
-    if instance_queue:
-        i = instance_queue.pop(0)
-        print('Launching instance {}'.format(i.id))
-        listen, send = Pipe()
-        p = Process(target=instance.Instance, args=(i.id, i.total_shards, i.shard_ids, send,))
-        instances.append(p)
-        p.start()
+class Shard:
+    def __init__(self, manager, instance_id: int):
+        self._manager = manager
+        self.instance_id: int = instance_id
 
-        if listen.recv() == 1:
-            launch_next_shard()
-        listen.close()
-    else:
-        print('All instances launched!')
+        shard_start: int = instance_id * manager.shards_per_instance
+        shard_finish: int = min(shard_start + manager.shards_per_instance, manager.total_shards)
+        self.shard_ids: list = list(range(shard_start, shard_finish))
+
+        p, c = Pipe()
+        self._process: Process = Process(target=Instance,
+                                         args=(self.instance_id, self._manager.total_shards, self.shard_ids, c,))
+        self._pipeline_parent: Connection = p
+        self._pipeline_child: Connection = c
+        # The child pipeline is used exclusively by the child process.
+        # We only store a reference so we can close it later.
+
+        self.deployed = False
+        log('shard', 'debug', 'Spawned shard with the following options: instance_id={}, shard_ids={}, ', self.instance_id, self.shard_ids)
+
+    @property
+    def is_alive(self):
+        return self._process.is_alive
+
+    def send(self, data: object):
+        self._pipeline_parent.send(data)
+
+    def receive(self, timeout: int = 5):
+        if not self._pipeline_parent.poll(timeout):
+            return None
+
+        return self._pipeline_parent.recv()
+
+    def start(self):
+        if self.is_alive():
+            self._process.kill()
+            self.deployed = False
+
+        self._process.start()
+        self.deployed = True
+
+    def shutdown(self):
+        self._process.terminate()
+        self._pipeline_parent.close()
+        self._pipeline_child.close()
 
 
-def wait(delay: int):
-    loop.run_until_complete(asyncio.sleep(delay))
+class Manager:
+    def __init__(self, total_shards: int, max_instances: int = cpu_count()):
+        self.total_shards: int = total_shards
+        self.max_instances: int = max_instances
+        self.shards_per_instance: int = math.ceil(total_shards / max_instances)
+
+        self._monitor_thread = Thread(target=self.monitor_shards)
+        self.shards = {}
+
+    def deploy(self):
+        if not self._monitor_thread.is_alive():
+            self._monitor_thread.start()
+
+        for i in range(self.max_instances):
+            log('manager', 'info', 'Launching instance {}', i)
+            self.spawn(i)
+            resp = self.shards[i].receive(None)
+
+            if resp is None:
+                log('manager', 'warning', 'Expected a response from instance {} but received None!', i)
+                continue
+
+            if resp['op'] == 'status' and resp['d'] == 'CONNECTED':
+                log('manager', 'info', 'Instance {} successfully deployed; all shards connected.', i)
+
+    def spawn(self, shard_id: int):
+        if shard_id in self.shards:
+            self.shards[shard_id].shutdown()
+
+        self.shards[shard_id] = Shard(self, shard_id)
+        self.shards[shard_id].start()
+
+    def monitor_shards(self):
+        while True:
+            for shard_id, shard in self.shards.items():
+                if shard.deployed and not shard.is_alive:
+                    log('manager', 'warning', 'Instance {} is marked as deployed, but the process is not alive. Restarting...', i)
+                    self.spawn(shard_id)
+
+            sleep(10)  # Check every 10 seconds.
 
 
 if __name__ == '__main__':
-    shards_per_instance = 1
-    total_shards = 2
-
-    sharded = '--sharded' in sys.argv
-
-    if sharded:
-        print('Sharding enabled. Validating shard count...')
-        if total_shards >= 40 and total_shards % 16 != 0:  # 40 * 2,500 = 100,000 (see: https://github.com/discordapp/discord-api-docs/issues/387)
-            print('Bad shard count: total_shards must be a multiple of 16')
-            sys.exit(0)
-
-        total_instances = math.ceil(total_shards / shards_per_instance)
-        print('Using {} instances'.format(total_instances))
-
-        wait(5)
-
-        for i in range(0, total_instances):
-            start = i * shards_per_instance
-            last = min(start + shards_per_instance, total_shards)
-            ids = list(range(start, last))
-
-            print('Appending instance {} to launch queue...'.format(i))
-            instance_queue.append(PendingInstance(i, total_shards, ids))
-    else:
-        instance_queue.append(PendingInstance(0, 1, [0], launch_next_shard))
-
-    launch_next_shard()
-
-    try:
-        while True:  # Keep the main process up
-            wait(5)
-    except KeyboardInterrupt:
-        pass
+    manager = Manager(10, 5)
+    manager.deploy()
--- instance.py
+++ instance.py
@@ -3,13 +3,15 @@ import os
 from datetime import datetime
 
 import discord
-from discord.ext.commands import when_mentioned_or as wmo
 from discord.ext.commands import AutoShardedBot
+from discord.ext.commands import when_mentioned_or as wmo
 
 
 class Instance:
     def __init__(self, instance, shard_count, ids, pipe):
         self.pipe = pipe
+        self._shard_ids = list(ids)
+        self._deployed = False
 
         with open('config.json') as r:
             config = json.load(r)
@@ -22,7 +24,11 @@ class Instance:
         self.bot.startup = datetime.now()
 
         self.bot.add_listener(self.on_ready)
+        self.bot.add_listener(self.on_shard_ready)
         self.bot.run(config.get('token'))
+
+    async def on_shard_ready(self, shard_id):
+        print('Shard id {} logged in.'.format(shard_id))
 
     async def on_ready(self):
         for file in os.listdir("cogs"):
@@ -30,13 +36,13 @@ class Instance:
                 name = file[:-3]
                 self.bot.load_extension(f"cogs.{name}")
 
-        print(f'[Instance-{self.bot.instance}] Ready!')
-        self.pipe.send(1)
-        self.pipe.close()        
+        if self._shard_ids == list(self.bot.shards.keys()):
+            self._deployed = True
+            self.pipe.send({'op': 'status', 'd': 'CONNECTED'})
 
     async def on_message(self, msg):
         if not self.bot.is_ready() or msg.author.bot or \
                 not (isinstance(msg.channel, discord.DMChannel) or msg.channel.permissions_for(msg.guild.me).send_messages):
-                return
+            return
 
         await self.bot.process_commands(msg)
